481c1e16a9c35cee944b1ffdcec880f4d6fa6835
Abhijit Nanda <abhijit.nanda@hyscaler.com>
2025-02-12 21:57:41 +0530

    About Us Page Done

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Footer from "./Footer";
+import Aboutus from "./Aboutus";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Aboutus />
       <Footer />
     </>
   );
