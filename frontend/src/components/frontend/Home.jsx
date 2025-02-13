40084d340b743a488a631901d946f098b811c046
Abhijit Nanda <abhijit.nanda@hyscaler.com>
2025-02-13 21:04:45 +0530

    UI done

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -3,12 +3,18 @@ import Hero from "./Hero";
 import Footer from "./Footer";
 import Aboutus from "./Aboutus";
 import Header from "./Header";
+import OurServices from "./OurServices";
+import OurProjects from "./OurProjects";
+import WhyChooseUs from "./WhyChooseUs";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Aboutus />
+      <OurServices />
+      <WhyChooseUs />
+      <OurProjects />
     </>
   );
 };
