816a2b18df94c6ef5ac20b699b2b49aca28bc86c
Abhijit Nanda <abhijitnanda8249@gmail.com>
2025-02-23 13:01:26 +0530

    Navbar Navigation links ui done

--- frontend/src/components/frontend/Header.jsx
+++ frontend/src/components/frontend/Header.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const path = useLocation();
+  const getNavClass = (currentPath) => {
+    return currentPath === path.pathname ? "active" : "";
+  };
   return (
     <header>
       <div className="container py-3">
@@ -13,22 +18,25 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/" className="nav-link">
+              <Nav.Link href="/" className={getNavClass("/")}>
                 Home
               </Nav.Link>
-              <Nav.Link href="/about" className="nav-link">
+              <Nav.Link href="/about" className={getNavClass("/about")}>
                 About Us
               </Nav.Link>
-              <Nav.Link href="/services" className="nav-link">
+              <Nav.Link href="/services" className={getNavClass("/services")}>
                 Services
               </Nav.Link>
-              <Nav.Link href="/projects" className="nav-link">
+              <Nav.Link href="/projects" className={getNavClass("/projects")}>
                 Projects
               </Nav.Link>
-              <Nav.Link href="/blogs" className="nav-link">
+              <Nav.Link href="/blogs" className={getNavClass("/blogs")}>
                 Blogs
               </Nav.Link>
-              <Nav.Link href="/contact-us" className="nav-link">
+              <Nav.Link
+                href="/contact-us"
+                className={getNavClass("/contact-us")}
+              >
                 Contact Us
               </Nav.Link>
             </Nav>
