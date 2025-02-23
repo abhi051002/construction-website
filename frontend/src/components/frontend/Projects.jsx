d295142d2f912c755fa07e71e0b81f4845b072a3
Abhijit Nanda <abhijitnanda8249@gmail.com>
2025-02-23 13:00:27 +0530

    Projects Page Done

---
+++ frontend/src/components/frontend/Projects.jsx
@@ -0,0 +1,180 @@
+import React from 'react';
+import ProjectsImg from "../../assets/images/construction2.jpg";
+
+
+const Projects = () => {
+    return (
+        <main>
+            <section className="section-7">
+                <div className="hero d-flex align-items-center">
+                    <div className="container">
+                        <div className="text-left">
+                            <span>Quality, Integrity, Value.</span>
+                            <h1>
+                                Our Projects
+                            </h1>
+                            <p>
+                                We excel at transforming visions into reality <br /> through outstanding
+                                craftsmanship and precise .
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="section-3 bg-light py-5">
+                <div className="container py-5">
+                    <div className="section-header text-center">
+                        <span>Our Projects</span>
+                        <h2>Discover our diverse range of projects</h2>
+                        <p>
+                            We offer a diverse array of construction services, spanning
+                            residential, commercial, and industrial projects.
+                        </p>
+                    </div>
+                    <div className="row pt-4">
+                        <div className="col-md-4 col-lg-4">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ProjectsImg} alt="" className="w-100" />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Odisha Projects</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Specialty construction is a niche sector within the
+                                            construction industry that focuses on projects requiring
+                                            specialized skills, materials, and techniques. Unlike
+                                            general construction
+                                        </p>
+                                    </div>
+                                    <a href="#" className="btn btn-primary small-btn">
+                                        Read More
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-lg-4">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ProjectsImg} alt="" className="w-100" />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Odisha Projects</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Specialty construction is a niche sector within the
+                                            construction industry that focuses on projects requiring
+                                            specialized skills, materials, and techniques. Unlike
+                                            general construction
+                                        </p>
+                                    </div>
+                                    <a href="#" className="btn btn-primary small-btn">
+                                        Read More
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-lg-4">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ProjectsImg} alt="" className="w-100" />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Odisha Projects</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Specialty construction is a niche sector within the
+                                            construction industry that focuses on projects requiring
+                                            specialized skills, materials, and techniques. Unlike
+                                            general construction
+                                        </p>
+                                    </div>
+                                    <a href="#" className="btn btn-primary small-btn">
+                                        Read More
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-lg-4">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ProjectsImg} alt="" className="w-100" />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Odisha Projects</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Specialty construction is a niche sector within the
+                                            construction industry that focuses on projects requiring
+                                            specialized skills, materials, and techniques. Unlike
+                                            general construction
+                                        </p>
+                                    </div>
+                                    <a href="#" className="btn btn-primary small-btn">
+                                        Read More
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-lg-4">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ProjectsImg} alt="" className="w-100" />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Odisha Projects</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Specialty construction is a niche sector within the
+                                            construction industry that focuses on projects requiring
+                                            specialized skills, materials, and techniques. Unlike
+                                            general construction
+                                        </p>
+                                    </div>
+                                    <a href="#" className="btn btn-primary small-btn">
+                                        Read More
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-lg-4">
+                            <div className="item">
+                                <div className="service-image">
+                                    <img src={ProjectsImg} alt="" className="w-100" />
+                                </div>
+                                <div className="service-body">
+                                    <div className="service-title">
+                                        <h3>Odisha Projects</h3>
+                                    </div>
+                                    <div className="service-content">
+                                        <p>
+                                            Specialty construction is a niche sector within the
+                                            construction industry that focuses on projects requiring
+                                            specialized skills, materials, and techniques. Unlike
+                                            general construction
+                                        </p>
+                                    </div>
+                                    <a href="#" className="btn btn-primary small-btn">
+                                        Read More
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default Projects
