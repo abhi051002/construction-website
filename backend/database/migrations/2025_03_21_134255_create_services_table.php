427481d2a0ea33272d4059fb3a61b8b250b5a39e
Abhijit Nanda <abhijitnanda8249@gmail.com>
2025-03-22 20:21:49 +0530

    Add soft deletes to services table migration

--- backend/database/migrations/2025_03_21_134255_create_services_table.php
+++ backend/database/migrations/2025_03_21_134255_create_services_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->text("content")->nullable();
             $table->string("image")->nullable();
             $table->integer("status")->default(1);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
