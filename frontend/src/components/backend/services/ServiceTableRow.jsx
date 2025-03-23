1040038819412c2ba33af909ec79563d221d54ea
Abhijit Nanda <abhijitnanda8249@gmail.com>
2025-03-23 18:59:47 +0530

    Add delete functionality to ServiceTableRow component

--- frontend/src/components/backend/services/ServiceTableRow.jsx
+++ frontend/src/components/backend/services/ServiceTableRow.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import useGetToken from "../../../hooks/useGetToken";
 
-const ServiceTableRow = ({ service, columns, windowWidth }) => {
+const ServiceTableRow = ({ service, columns, windowWidth, onDelete }) => {
+  const { token } = useGetToken();
+  const handleDelete = async () => {
+    if (confirm("Are you sure you want to delete?")) {
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/services/${service.id}`,
+        {
+          method: "DELETE",
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const result = await response.json();
+      if (result.status) {
+        onDelete();
+        toast.success(result.message);
+      } else {
+        toast.error(result.message);
+      }
+    }
+  };
   return (
     <tr>
       {columns.map((column) => {
@@ -27,12 +52,19 @@ const ServiceTableRow = ({ service, columns, windowWidth }) => {
             return (
               <td key={column.key}>
                 <div className="d-flex flex-wrap">
-                  <a href="" className="btn btn-primary btn-sm me-1 mb-1">
+                  <Link
+                    to={`/admin/services/edit/${service.id}`}
+                    className="btn btn-primary btn-sm me-1 mb-1"
+                  >
                     {windowWidth < 400 ? "E" : "Edit"}
-                  </a>
-                  <a href="" className="btn btn-secondary btn-sm mb-1">
+                  </Link>
+                  <Link
+                    to="#"
+                    onClick={handleDelete}
+                    className="btn btn-secondary btn-sm mb-1"
+                  >
                     {windowWidth < 400 ? "D" : "Delete"}
-                  </a>
+                  </Link>
                 </div>
               </td>
             );
