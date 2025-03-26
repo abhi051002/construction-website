67999381c5f724965ed6fe6de06d36fed32fe704
Abhijit Nanda <abhijitnanda8249@gmail.com>
2025-03-26 20:40:24 +0530

    Add routes for managing articles in the admin panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,6 +21,9 @@ import { default as EditService } from "./components/backend/services/Edit";
 import { default as ShowProjects } from "./components/backend/projects/Show";
 import { default as CreateProjects } from "./components/backend/projects/Create";
 import { default as EditProjects } from "./components/backend/projects/Edit";
+import { default as ShowArticles } from "./components/backend/articles/Show";
+import { default as CreateArticles } from "./components/backend/articles/Create";
+import { default as EditArticles } from "./components/backend/articles/Edit";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -54,6 +57,9 @@ function App() {
           <Route path="/admin/projects" element={<ShowProjects />} />
           <Route path="/admin/projects/create" element={<CreateProjects />} />
           <Route path="/admin/projects/edit/:id" element={<EditProjects />} />
+          <Route path="/admin/articles" element={<ShowArticles />} />
+          <Route path="/admin/articles/create" element={<CreateArticles />} />
+          <Route path="/admin/articles/edit/:id" element={<EditArticles />} />
 
           <Route path="*" element={<NotFound />} />
         </Routes>
