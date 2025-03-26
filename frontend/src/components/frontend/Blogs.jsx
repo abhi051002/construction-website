e241bd49543d9daf1bd46e8e643bbb4e88ff5f47
Abhijit Nanda <abhijitnanda8249@gmail.com>
2025-03-26 20:40:03 +0530

    Update Blogs component to set BlogNews limit to 'all' and improve formatting

--- frontend/src/components/frontend/Blogs.jsx
+++ frontend/src/components/frontend/Blogs.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import BlogNews from './BlogNews'
+import React from "react";
+import BlogNews from "./BlogNews";
 
 const Blogs = () => {
-    return (
-        <main>
-            <section className="section-7">
-                <div className="hero d-flex align-items-center">
-                    <div className="container">
-                        <div className="text-left">
-                            <span>Quality, Integrity, Value.</span>
-                            <h1>
-                                Blogs
-                            </h1>
-                            <p>
-                                We excel at transforming visions into reality <br /> through outstanding
-                                craftsmanship and precise .
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <BlogNews />
-        </main>
-    )
-}
+  return (
+    <main>
+      <section className="section-7">
+        <div className="hero d-flex align-items-center">
+          <div className="container">
+            <div className="text-left">
+              <span>Quality, Integrity, Value.</span>
+              <h1>Blogs</h1>
+              <p>
+                We excel at transforming visions into reality <br /> through
+                outstanding craftsmanship and precise .
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <BlogNews limit="all" />
+    </main>
+  );
+};
 
-export default Blogs
+export default Blogs;
