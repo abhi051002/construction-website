b535b906ebb0fd501a324ef8b44a8d766d9d8c94
Abhijit Nanda <abhijitnanda8249@gmail.com>
2025-03-27 22:06:49 +0530

    Add Create component for managing testimonials

---
+++ frontend/src/components/backend/testimonials/Create.jsx
@@ -0,0 +1,194 @@
+import React, { useState, useRef, useMemo } from "react";
+import Sidebar from "../../Sidebar";
+import { ArrowLeftCircle, Briefcase, FolderOpen } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import useGetToken from "../../../hooks/useGetToken";
+import { toast } from "react-toastify";
+
+const Create = () => {
+  const [isDisable, setIsDisable] = useState(false);
+  const [imageId, setImageId] = useState(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [loading, setLoading] = useState(false);
+  const { token } = useGetToken();
+  const navigate = useNavigate();
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+      const newData = { ...data, imageId: imageId };
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/testimonials`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(newData),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const responseData = await response.json();
+      if (responseData.status) {
+        toast.success(responseData.message);
+        navigate("/admin/testimonials");
+      } else {
+        toast.error(responseData.error.slug[0]);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const handleFile = async (e) => {
+    const formData = new FormData();
+    const file = e.target.files[0];
+    formData.append("image", file);
+    await fetch(`${import.meta.env.VITE_BACKEND_URL}/temp-images`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.status == false) {
+          toast.error(result.error.image[0]);
+        } else {
+          setImageId(result.data.id);
+        }
+      });
+  };
+  return (
+    <main className="dashboard-container bg-light min-vh-100 py-4">
+      <div className="container">
+        <div className="row g-4">
+          <div className="col-lg-3">
+            {/* Sidebar */}
+            <Sidebar activePage="testimonials" />
+          </div>
+          <div className="col-md-9">
+            <div className="card shadow border-0">
+              <div className="card-body p-4">
+                <div className="d-flex justify-content-between">
+                  <h4 className="h-5 d-flex">
+                    <FolderOpen size={28} className="me-2" />
+                    {`Testimonials > Create`}
+                  </h4>
+                  <Link
+                    to="/admin/testimonials"
+                    className="btn btn-primary d-flex"
+                  >
+                    <ArrowLeftCircle className="me-2" />
+                    Back
+                  </Link>
+                </div>
+                <hr />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="mb-3">
+                    <label htmlFor="" className="form-label">
+                      Testimonial
+                    </label>
+                    <textarea
+                      placeholder="Testimonials"
+                      className={`form-control ${
+                        errors.testimonial ? "is-invalid" : ""
+                      }`}
+                      {...register("testimonial", {
+                        required: "This testimonial field is required",
+                      })}
+                    ></textarea>
+                    {errors.testimonial && (
+                      <p className="invalid-feedback">
+                        {errors?.testimonial?.message}
+                      </p>
+                    )}
+                  </div>
+                  <div className="col-md-6">
+                    <div className="mb-3">
+                      <label htmlFor="" className="form-label">
+                        Citation
+                      </label>
+                      <input
+                        type="text"
+                        className={`form-control ${
+                          errors.citation ? "is-invalid" : ""
+                        }`}
+                        placeholder="Enter Citation"
+                        {...register("citation", {
+                          required: "This citation field is required",
+                        })}
+                      />
+                      {errors.citation && (
+                        <p className="invalid-feedback">
+                          {errors?.citation?.message}
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label htmlFor="" className="form-label">
+                          Designation
+                        </label>
+                        <input
+                          type="text"
+                          className={`form-control`}
+                          placeholder="Enter Citation"
+                          {...register("designation", {
+                            required: "This designation field is required",
+                          })}
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="mb-3">
+                        <label htmlFor="" className="form-label">
+                          Status
+                        </label>
+                        <select
+                          className="form-control"
+                          {...register("status")}
+                        >
+                          <option value="1">Active</option>
+                          <option value="0">Block</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="" className="form-label">
+                      Image
+                    </label>
+                    <br />
+                    <input type="file" onChange={handleFile} />
+                  </div>
+                  <button
+                    className="btn btn-primary w-100"
+                    disabled={isDisable}
+                  >
+                    {loading ? "Creating..." : "Create"}
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Create;
